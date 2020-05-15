81ab12fcc3006ec4c98e824df56752e2bdaa0eff
xavier.reixach <xavierreixac@gmail.com>
2020-05-15 10:02:16 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,6 +44,11 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'content' => 'required|max:191',
+            'status' => 'required|max:10',
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
         $task->status = $request->status;
@@ -91,6 +96,11 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'content' => 'required|max:191',
+            'status' => 'required|max:10',
+        ]);
+        
         $task = Task::find($id);
         $task->content = $request->content;
         $task->status = $request->status;
